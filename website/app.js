d9babc94ffcc8745714d11009555f65eff36d3e5
ahmedsamir96m <ahmedsamir96m@gmail.com>
2020-09-29 15:34:36 +0200

    initialize the apiKet and baseURL, changing date variable name to currentDate instead of d

--- website/app.js
+++ website/app.js
@@ -1,5 +1,12 @@
 /* Global Variables */
+const apiKey = "f3dd4d99772d41e15b26f5fc6a2b683c";
+const baseURL = `https://api.openweathermap.org/data/2.5/forecast?zip=`;
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+let currentDate = new Date();
+let newDate =
+  currentDate.getMonth() +
+  "." +
+  currentDate.getDate() +
+  "." +
+  currentDate.getFullYear();
