f83267d31fbfd7998afbbb4542f3436e679e0cae
ahmedsamir96m <ahmedsamir96m@gmail.com>
2020-09-29 14:43:29 +0200

    setting up the local server on port 8080

--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,3 +19,9 @@ app.use(bodyParser.json());
 app.use(express.static("website"));
 
 // Setup Server
+const port = 8080;
+const listening = () => {
+  console.log(`running of localhost on port ${port}`);
+};
+
+const server = app.listen(port, listening);
